32475d83078e7009fdfe3d77fa999b195b6d975b
wyz <781593022@qq.com>
2021-03-07 11:42:23 +0800

    提交

---
+++ ems_parent/ems_user/src/main/java/cn/edu/zstu/UserApplication.java
@@ -0,0 +1,19 @@
+package cn.edu.zstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author wyz
+ * @Date 2021/3/4 20:15
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
